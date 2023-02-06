6e054753dfcfe3a199dd7a673d7300d55015cf3f
wajdioppa00 <116859764+PARKWAJDI@users.noreply.github.com>
2023-02-06 12:01:02 +0800

    Add files via upload

---
+++ TriangleDLL/TriangleDLL/TriangleByPointDLL.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//this coding is used to determine various parameters such as
+//area, perimeter and angle of a triangle by using the side value given by the user
+
+namespace TriangleDLL
+{
+    public class TriangleByPointDLL //class by point
+    {
+        private double _x1;
+        private double _y1;
+        private double _x2;
+        private double _y2;
+        private double _x3;
+        private double _y3;
+
+
+        public double X1
+        {
+            get { return _x1; }
+            set { _x1 = value; }
+        }
+
+        public double Y1
+        {
+            get { return _y1; }
+            set { _y1 = value; }
+        }
+
+        public double X2
+        {
+            get { return _x2; }
+            set { _x2 = value; }
+        }
+
+        public double Y2
+        {
+            get { return _y2; }
+            set { _y2 = value; }
+        }
+
+        public double X3
+        {
+            get { return _x3; }
+            set { _x3 = value; }
+        }
+
+        public double Y3
+        {
+            get { return _y3; }
+            set { _y3 = value; }
+        }
+
+        /// <summary>
+        /// contructor for triangle calculator by point (3 coordinates, 6 unknowns)
+        /// one constructor only because there are no parameter (=0)
+        /// </summary>
+        public TriangleByPointDLL() //constructor
+        {
+            _x1 = 0;
+            _y1 = 0;
+            _x2 = 0;
+            _y2 = 0;
+            _x3 = 0;
+            _y3 = 0;
+
+        }
+
+        /// <summary>
+        /// there are various parameters can be calculate by using specific formula (side, perimeter, area, angle)
+        /// </summary>
+        /// <param name="x1"> The first unknown of coordinate 1 of a triangle </param> 
+        /// <param name="y1"> The second unknown of coordinate 1 of a triangle </param> 
+        /// <param name="x2"> The first unknown of coordinate 2 of a triangle </param>
+        /// <param name="y2"> The second unknown of coordinate 2 of a triangle </param> 
+        /// <param name="x3"> The first unknown of coordinate 3 of a triangle </param> 
+        /// <param name="y3"> The second unknown of coordinate 3 of a triangle </param> 
+        /// <returns> the formula for each parameters using x1, x2, x3, y1, y2, and y3 </returns>
+        public double SideA() //method to find distance of side A by using pythagoras
+        {
+            return (Math.Sqrt(((_x2 - _x1) * (_x2 - _x1)) + ((_y2 - _y1) * (_y2 - _y1))));
+        }
+        public double SideB() //method to find distance of side B by using pythagoras
+        {
+            return (Math.Sqrt(((_x1 - _x3) * (_x1 - _x3)) + ((_y1 - _y3) * (_y1 - _y3))));
+        }
+        public double SideC() //method to find distance of side C by using pythagoras
+        {
+            return (Math.Sqrt(((_x3 - _x2) * (_x3 - _x2)) + ((_y3 - _y2) * (_y3 - _y2))));
+        }
+        public double Perimeter() //method to find triangle perimeter
+        {
+            return (Math.Sqrt(((_x2 - _x1) * (_x2 - _x1)) + ((_y2 - _y1) * (_y2 - _y1))))
+                + (Math.Sqrt(((_x1 - _x3) * (_x1 - _x3)) + ((_y1 - _y3) * (_y1 - _y3))))
+                + (Math.Sqrt(((_x3 - _x2) * (_x3 - _x2)) + ((_y3 - _y2) * (_y3 - _y2))));
+
+        }
+
+        private double S() //method to find triangle half perimeter
+        {
+            return Perimeter() / 2;
+
+            // return Perimeter / 2; 
+        }
+
+        public double Area() //method to find triangle area
+        {
+            return Math.Sqrt(S() * (S() - SideA()) * (S() - SideB()) * (S() - SideC()));
+
+            //return Math.Sqrt(S*(S-SideA)* (S - SideB)* (S - SideC));
+        }
+
+        //for angleBC
+        public double AngleBC() //method to find angle between side B and side C
+        {
+            return ((Math.Acos((SideB() * SideB() + SideC() * SideC() - SideA() * SideA()) / (2 * SideB() * SideC()))) * 180) / Math.PI;
+        }
+
+        //for angleAC
+        public double AngleAC() //method to find angle between side A and side C
+        {
+            return ((Math.Acos((SideA() * SideA() + SideC() * SideC() - SideB() * SideB()) / (2 * SideA() * SideC()))) * 180) / Math.PI;
+        }
+
+        //for angleAB
+        public double AngleAB() //method to find angle between side A and side B
+        {
+            return ((Math.Acos((SideA() * SideA() + SideB() * SideB() - SideC() * SideC()) / (2 * SideA() * SideB()))) * 180) / Math.PI;
+        }
+
+        public string Reality() //method to determine the reality of the triangle
+        {
+            if ((SideA() > SideB()) && (SideA() > SideC())) //if side A was the longest
+            {
+                if (SideB() + SideC() > SideA())
+                {
+                    return "Real";
+                }
+                if (SideB() + SideC() == SideA())
+                {
+                    return "Null";
+                }
+
+                if (SideB() + SideC() < SideA())
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            if ((SideB() > SideA()) && (SideB() > SideC())) //if side B was the longest
+            {
+                if (SideA() + SideC() > SideB())
+                {
+                    return "Real";
+                }
+                if (SideA() + SideC() == SideB())
+                {
+                    return "Null";
+                }
+
+                if (SideA() + SideC() < SideB())
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            if ((SideC() > SideB()) && (SideC() > SideA())) //if side C was the longest
+            {
+                if (SideB() + SideA() > SideC())
+                {
+                    return "Real";
+                }
+                if (SideB() + SideA() == SideC())
+                {
+                    return "Null";
+                }
+
+                if (SideB() + SideA() < SideC())
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            return "";
+        }
+    }
+}
---
+++ TriangleDLL/TriangleDLL/TriangleBySideDLL.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//this coding is used to determine various parameters such as
+//area, perimeter and angle of a triangle by using the side value given by the user
+
+namespace TriangleDLL
+{
+    public class TriangleBySideDLL //class by side
+    {
+        private double _sideA; 
+        private double _sideB;
+        private double _sideC;
+
+        public double SideA
+        {
+            get { return _sideA; }
+            set { _sideA = value; }
+        }
+
+        public double SideB
+        {
+            get { return _sideB; }
+            set { _sideB = value; }
+        }
+
+        public double SideC
+        {
+            get { return _sideC; }
+            set { _sideC = value; }
+        }
+
+        /// <summary>
+        /// contructor for triangle calculator by sides (3 sides)
+        /// one constructor only because there are no parameter (=0)
+        /// </summary>
+        public TriangleBySideDLL() //constructor
+        {
+            _sideA = 0;
+            _sideB = 0;
+            _sideC = 0;
+        }
+
+        /// <summary>
+        /// there are various parameters can be calculate by using specific formula (side, perimeter, area, angle)
+        /// </summary>
+        /// <param name="sideA"> The first length of the triangle of the formula </param> 
+        /// <param name="sideB"> The second length of the triangle of the formula </param> 
+        /// <param name="sideC"> The third length of the triangle of the formula </param> 
+        /// <returns> the formula for each parameters using sideA, sideB, and sideC </returns>
+        public double Perimeter() //method to find triangle perimeter
+        {
+            return _sideA + _sideB + _sideC;
+        }
+
+        private double S() //method to find triangle half perimeter
+        {
+            //return (_sideA + _sideB + _sideC)/2;
+
+            return Perimeter() / 2;
+        }
+
+        public double Area() //method to find triangle area
+        {
+            /*return Math.Sqrt(((_sideA + _sideB + _sideC) / 2) 
+                * (((_sideA + _sideB + _sideC) / 2) - _sideA) 
+                * (((_sideA + _sideB + _sideC) / 2) - _sideB) 
+                * (((_sideA + _sideB + _sideC) / 2) - _sideC)); */
+
+            return Math.Sqrt(S() * (S() - _sideA) * (S() - _sideB) * (S() - _sideC));
+        }
+
+        //for angleBC
+        public double AngleBC() //method to find angle between side B and side C
+        {
+            return ((Math.Acos(((_sideB * _sideB) + (_sideC * _sideC) - (_sideA * _sideA)) / (2 * _sideB * _sideC))) * 180) / Math.PI;
+        }
+
+        //for angleAC
+        public double AngleAC() //method to find angle between side A and side C
+        {
+            return ((Math.Acos(((_sideA * _sideA) + (_sideC * _sideC) - (_sideB * _sideB)) / (2 * _sideA * _sideC))) * 180) / Math.PI;
+        }
+
+        //for angleAB
+        public double AngleAB() //method to find angle between side A and side B
+        {
+            return ((Math.Acos(((_sideA * _sideA) + (_sideB * _sideB) - (_sideC * _sideC)) / (2 * _sideA * _sideB))) * 180) / Math.PI;
+        }
+
+        public string Reality() //method to determine the reality of the triangle
+        {
+            if ((_sideA > _sideB) && (_sideA > _sideC)) //if side A was the longest
+            {
+                if (_sideB + _sideC > _sideA)
+                {
+                    return "Real";
+                }
+                if (_sideB + _sideC == _sideA)
+                {
+                    return "Null";
+                }
+
+                if (_sideB + _sideC < _sideA)
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            if ((_sideB > _sideA) && (_sideB > _sideC)) //if side B was the longest
+            {
+                if (_sideA + _sideC > _sideB)
+                {
+                    return "Real";
+                }
+                if (_sideA + _sideC == _sideB)
+                {
+                    return "Null";
+                }
+
+                if (_sideA + _sideC < _sideB)
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            if ((_sideC > _sideB) && (_sideC > _sideA)) //if side C was the longest
+            {
+                if (_sideB + _sideA > _sideC)
+                {
+                    return "Real";
+                }
+                if (_sideB + _sideA == _sideC)
+                {
+                    return "Null";
+                }
+
+                if (_sideB + _sideA < _sideC)
+                {
+                    return "Imaginary";
+                }
+
+            }
+
+            return "";
+        }
+
+    }
+}
